3236c5226602158b3c382ae26744c3563e45ae9b
Angelikikark <167430380+Angelikikark@users.noreply.github.com>
2024-10-03 20:07:30 +0100

    Update Java script

--- script.js
+++ script.js
@@ -1,57 +1,53 @@
-   <script>
-        function displayTemperature(response) {
-          let temperatureElement = document.querySelector(
-            "#current-temperature"
-          );
-          let temperature = Math.round(response.data.temperature.current);
-          let cityElement = document.querySelector("#current-city");
-          cityElement.innerHTML = response.data.city;
-          temperatureElement.innerHTML = temperature;
-        }
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
 
-        function search(event) {
-          event.preventDefault();
-          let searchInputElement = document.querySelector("#search-input");
-          let city = searchInputElement.value;
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
 
-          let apiKey = "t7o26f41bc212fa3436aa27360809eab";
-          let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "t7o26f41bc212fa3436aa27360809eab";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-          axios.get(apiUrl).then(displayTemperature);
-        }
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-        function formatDate(date) {
-          let minutes = date.getMinutes();
-          let hours = date.getHours();
-          let day = date.getDay();
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
 
-          if (minutes < 10) {
-            minutes = `0${minutes}`;
-          }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-          if (hours < 10) {
-            hours = `0${hours}`;
-          }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
-          let days = [
-            "Sunday",
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-          ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-          let formattedDay = days[day];
-          return `${formattedDay} ${hours}:${minutes}`;
-        }
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
 
-        let searchForm = document.querySelector("#search-form");
-        searchForm.addEventListener("submit", search);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
 
-        let currentDateELement = document.querySelector("#current-date");
-        let currentDate = new Date();
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
 
-        currentDateELement.innerHTML = formatDate(currentDate);
-      </script>
+currentDateELement.innerHTML = formatDate(currentDate);
