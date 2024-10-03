7c76cd55c7c7d7396f8eec3bc3435aeb37271f66
Angelikikark <167430380+Angelikikark@users.noreply.github.com>
2024-10-03 21:20:30 +0100

    Update intex and script

--- intex.js
+++ intex.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "t7o26f41bc212fa3436aa27360809eab";
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
